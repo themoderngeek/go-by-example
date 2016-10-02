68e0e9575762dd52db7dd6d8283c8ece6fd360df
Mark Taylor <moderngeekish@gmail.com>
2016-10-02 15:04:13 +0100

    Looking at values in go
    Following and updating the Values tutorial https://gobyexample.com/values

---
+++ values/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello " + "world" + "!")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("10.0/2.5 =", 10.0/2.5)
+
+	fmt.Println("false && true =", false && true)
+	fmt.Println("false || true =", false || true)
+	fmt.Println("!false =", !false)
+}
